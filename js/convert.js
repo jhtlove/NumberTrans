8493afb9ce7ba6648144bffbfde1c650bea60b94
jhtlove <weijiecoming@126.com>
2018-09-18 21:32:02 +0800

    ok lalala

--- js/convert.js
+++ js/convert.js
@@ -1,3 +1,7 @@
+$(function () {
+    $("#inputNumber")[0].focus();
+});
+
 function convertNumber(number) {
     if (0 <= number && number <= 9) {
         //零、壹、贰、叁、肆、伍、陆、柒、捌、玖
@@ -35,9 +39,11 @@ function getCounting(number) {
     var pointPos = numberStr.indexOf(".");
     var integerPart = "";
     var result = "";
+    var yuan = "";
     if (pointPos != -1) {
         //001.00
         //0.34
+        yuan = "元";
         integerPart = parseInt(numberStr.substring(0, pointPos));
         var decimalPart = numberStr.substring(pointPos + 1);
         var resultDec = "";
@@ -48,17 +54,17 @@ function getCounting(number) {
         if (parseInt(decimalPart)) {
             resultDec = parseDecimalPart(decimalPart);
         }
-        result = resultInteger + resultDec;
+        result = resultInteger + yuan + resultDec;
         console.log(result);
-        if (result = "") {
+        if (result == "") {
             result = "零元整";
         }
         return result;
     } else {
         integerPart = parseInt(number);
-        var lastUnit = "元整";
+        yuan = "元整";
         result = parseIntPart(integerPart);
-        result = result + lastUnit;
+        result = result + yuan;
         console.log(result);
         return result;
     }
@@ -68,7 +74,7 @@ function parseIntPart(int) {
     if (int == 0) {
         return "零";
     }
-    var unit = ["", "拾", "伯", "仟"]; //四位一级
+    var unit = ["", "拾", "佰", "仟"]; //四位一级
     var unit2 = ["", "万", "亿", "兆"];
     var number = int;
     var numStr = "" + int;
@@ -88,25 +94,37 @@ function parseIntPart(int) {
     var index = 0;
     //var parts = [];
     // 从低位开始分解
-    for (var i = 0; i <numParts.length; i++) {
-        var theUnit = unit2[i]; //个 万 亿 兆
+    for (var i = 0; i < numParts.length; i++) {
+        var part = "";
+        var theUnit2 = unit2[i]; //个 万 亿 兆
         var thePart = numParts[i];//分解四位的
         // 四位 1000 100 10 取模 10 ？
         var partLen = thePart.length;
-        for (var j = 0; j < partLen; j++) {
+        var isZero = true; // 处理末尾的零
+        var hasMidZero = false;//处理中间的零
+        //末位开始拆分
+        for (var j = partLen - 1; j >= 0; j--) {
+            var theUnit = unit[partLen - 1 - j];
             var singleNum = parseInt(thePart.charAt(j)); //拆分个单个数字
             var theNumber = convertNumber(singleNum); //某一位数字
-            if(theNumber === "零"){
-                result = theNumber + result; //0 后面不带单位？
-            }else{
-                result = theNumber + theUnit + result;
+            if (singleNum == 0) {
+                // 末尾的0直接舍去
+                // 中间的0
+                if (!isZero) {
+                    if (!hasMidZero) {
+                        part = theNumber + part; //0 后面不带单位
+                    }
+                    hasMidZero = true;
+                }
+            } else {
+                part = theNumber + theUnit + part;
+                isZero = false;
             }
-            
-            //parts.push(singleNum);
         }
+        result = part + theUnit2 + result;
     }
-    result.replace("零仟","零");
-    result.replace("零百","");
+    // result.replace(/零仟/g, "零");
+    // result.replace(/零百/g, "零");
     //1 2345 0400
     //壹亿貳仟万叁伯万肆拾万伍万 零仟零伯零拾零   元  整
     //一亿两千三百四十五万元整
@@ -117,14 +135,17 @@ function parseIntPart(int) {
 function parseDecimalPart(decimal) {
     var unit = ["角", "分"];
     var result = "";
+    //从高位开始
     for (var j = 0; j < decimal.length; j++) {
         var singleNum = parseInt(decimal.charAt(j)); //拆分个单个数字
-        // var theNumber = convertNumber(singleNum);//某一位数字
-        //result = theNumber + theUnit + result;
+        var theNumber = convertNumber(singleNum);//某一位数字
         var theUnit = unit[j];
-        if (singleNum != 0) {
-
+        if(singleNum == 0){
+            result = result + theNumber; 
+        }else{
+            result = result + theNumber + theUnit; 
         }
+        
     }
-    return
+    return result;
 }
